36a8ec389a7f62ef37047b8b2536105d84f6643a
LeeHoSeop <ihoseob@LeeHoSeopui-MacBookPro.local>
2021-04-24 18:47:15 +0900

    오버워치 예제 테스트

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import './App.css';
+import img from './images/logo_overwatch.png';
+function App() {
+  return (
+    <div className="container">
+      <div className="heroes">
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+        <div className="hero">
+          <div className="image"></div>
+        </div>
+      </div>
+      <div className="logo">
+        <img src={img} alt="Overwatch" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
